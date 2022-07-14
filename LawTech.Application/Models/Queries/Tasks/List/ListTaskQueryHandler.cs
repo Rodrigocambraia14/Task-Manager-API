d50b1eaae1f80e3768f7a71ee2392d398414d3dc
Rodrigo Gonçalves Cambraia Soares <rodrigocambraia.jr@gmail.com>
2022-07-14 09:44:01 -0300

    Correção query de tasks

--- LawTech.Application/Models/Queries/Tasks/List/ListTaskQueryHandler.cs
+++ LawTech.Application/Models/Queries/Tasks/List/ListTaskQueryHandler.cs
@@ -29,6 +29,7 @@ namespace LawTech.Application.Models.Queries.Tasks.List
             var tasks = await this.defaultContext.Tasks.WhereIf(query.UserId is not null, x => x.UserId == query.UserId &&
                                                                                           x.Status != CrossCutting.Enums.TaskStatus.Deleted)
                                                        .ProjectTo<ListTaskQueryResponse>(this.mapper.ConfigurationProvider)
+                                                       .OrderBy(x => x.Priority).ThenByDescending(x => x.CreatedDate)
                                                        .ToListAsync(cancellationToken: cancellationToken);
 
             return ContractResponse.ValidContractResponse(string.Empty, tasks);
